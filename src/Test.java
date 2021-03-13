9df878cb374966c5f7708bc02e357d0d5309e3a3
deepak3952 <deepak3952@gmail.com>
2021-03-13 16:17:49 +0530

    Thrid commit

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main (String args []){
         System.out.println("This is my first program using intellij");
         System.out.println("Second commit test");
-        System.out.println("Third commit test");
+
     }
 }
